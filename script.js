e0a62eb7642a9fc33e4272af5fb7fcbe5702eeaa
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-10 16:44:20 -0400

    Test basic arithmatic / vars

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let ourPlanet = 'Earth';
-let currentVisitor;
+//basic arithmatic + variable assignment
+
+console.log(10 + 100);
+console.log (1 + 2, 1 + 3, 1 + 4, 1 + 5, 1 + 6);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage)
