3934d7e90ab042bd0466c85a92628834d672beed
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-11 11:47:00 -0400

    Test credential checker logic

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
-//conditional muscle memory
+// Credential check conditionals
 
-result = a || b;
+let userName = prompt("Who is it?", '');
 
-alert( true || true );   // true
-alert( false || true );  // true
-alert( true || false );  // true
-alert( false || false ); // false
+if (userName === 'admin') {
 
-let hour = 9;
+  let pass = prompt('Password?', '');
 
-if (hour < 10 || hour > 18) {
-  alert( 'The office is closed.' );
+  if (pass === '101TallGiraffes') {
+    alert( 'Welcome!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
 }
-
-
-hour = 12;
-let isWeekend = true;
-
-if (hour < 10 || hour > 18 || isWeekend) {
-  alert( 'The office is closed.' ); // it is the weekend
-}
-
-result = value1 || value2 || value3;
-
-alert( 1 || 0 ); // 1 (1 is truthy)
-
-alert( null || 1 ); // 1 (1 is the first truthy value)
-alert( null || 0 || 1 ); // 1 (the first truthy value)
-
-alert( undefined || null || 0 ); // 0 (all falsy, returns the last value)
-
-let firstName = "";
-let lastName = "";
-let nickName = "SuperCoder";
-
-alert( firstName || lastName || nickName || "Anonymous"); // SuperCoder
-
-true || alert("not printed");
-false || alert("printed");
-
-alert( true && true );   // true
-alert( false && true );  // false
-alert( true && false );  // false
-alert( false && false ); // false
-
-hour = 12;
-let minute = 30;
-
-if (hour == 12 && minute == 30) {
-  alert( 'The time is 12:30' );
-}
-
-// if the first operand is truthy,
-// AND returns the second operand:
-alert( 1 && 0 ); // 0
-alert( 1 && 5 ); // 5
-
-// if the first operand is falsy,
-// AND returns it. The second operand is ignored
-alert( null && 5 ); // null
-alert( 0 && "no matter what" ); // 0
