e7e9918f035385456ff962381b3c0f8b236984ea
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-11 20:05:03 -0400

    Test live DOM manipulation

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let firstName = 'Spongebob';
-let lastName = 'Squarepants';
-let fullName = `${firstName} ${lastName}`;
-let currentYear = 2022;
-let birthYear = 1999;
-let age = currentYear - birthYear;
-let greeting = `${fullName} was born in ${birthYear}. It is now ${currentYear}. This means he is ${age} years old!`
+document.addEventListener("DOMContentLoaded", () => { 
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
 
-console.log(greeting);
+textBox.addEventListener('keydown', (event) => output.textContent = `You pressed "${event.key}".`);
+});
