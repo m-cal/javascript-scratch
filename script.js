5ec66c1bad5ed7cdfd28603a5725d5f1b4ed3ffd
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-12 23:17:39 -0400

    Create math functions

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => { 
-const textBox = document.querySelector("#textBox");
-const output = document.querySelector("#output");
+function squared(num) {
+  return num * num;
+}
 
-textBox.addEventListener('keydown', (event) => output.textContent = `You pressed "${event.key}".`);
-});
+function cubed(num) {
+  return num * num * num;
+}
+
+function factorial(num) {
+  if (num < 0) return undefined;
+  if (num === 0) return 1;
+  let x = num - 1;
+  while (x > 1) {
+    num *= x;
+    x--;
+  }
+  return num;
+}
