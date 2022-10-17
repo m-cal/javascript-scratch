f5064116a3bdb9ca94df7c46009d99d8fd6b1fe0
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-16 21:30:32 -0400

    Make multi-line ternary FizzBuzz

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let age = prompt('What is your age?', 18);
+function FizzBuzz(num) {
+  (num % 3 == 0) && (num % 5 == 0) ? console.log('FizzBuzz') :
+  (num % 3 == 0) ? console.log('Fizz') :
+  (num % 5 == 0) ? console.log('Buzz') :
+  console.log(num);
+}
 
-let welcome = (age >= 18) ?
-  () => alert('Hello!') :
-  () => alert('Ask your parents first!');
-
-welcome();
+FizzBuzz(15);
