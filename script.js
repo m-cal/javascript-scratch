eeadda4772d040aa6100c3b8c84f4504d9e81ce2
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-20 22:40:17 -0400

    Make countdown && multicolor circle

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
- num = prompt('Enter a number to be FizzBuzz\'d');
-
-FizzBuzz(num);
-
-function FizzBuzz(num) {
-  let list = '';
-  
-  for (let i = 1; i <= num; i++) {
-    (i % 3 == 0) && (i % 5 == 0) ? list += ' FizzBuzz' :
-    (i % 3 == 0) ? list += ' Fizz':
-    (i % 5 == 0) ? list += ' Buzz':
-    list += ` ${i}`;
+const countdown = function() {
+  let timer = 10;
+  while (timer >= 0) {
+    if (timer === 10) {
+      console.log(`Countdown ${timer}`);
+    } else if (timer === 0) {
+      console.log(`Blast off!`);
+    } else {
+      console.log(timer);
+    }
+    timer--;
   }
-  alert(list);
 }
-
-
-
