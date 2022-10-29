39b2e956d0f18120e82b71b7964c58bc046266b2
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-29 00:00:19 -0400

    Test table creation and styling

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const countdown = function() {
-  let timer = 10;
-  while (timer >= 0) {
-    if (timer === 10) {
-      console.log(`Countdown ${timer}`);
-    } else if (timer === 0) {
-      console.log(`Blast off!`);
-    } else {
-      console.log(timer);
-    }
-    timer--;
-  }
-}
+let test = (132*2);
